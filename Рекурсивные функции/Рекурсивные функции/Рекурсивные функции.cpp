901340706be97cef4b23cbb1514b09beb71705f7
DUPRETAA <141547002+DUPRETAA@users.noreply.github.com>
2024-12-23 05:51:33 +0500

    Add files via upload

---
+++ Рекурсивные функции/Рекурсивные функции/Рекурсивные функции.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cmath>
+#include <clocale>
+
+using namespace std;
+
+int factorial(int count) 
+{
+	if (count == 1 || count == 0) return 1;
+	return (count * factorial(count - 1));
+}
+
+double get_sum(double num, int count)
+{
+	if (count  == 0) return 1;
+	return (((pow(count, 2) + 1) / (factorial(count)) * pow(num / 2, count)) + get_sum(num, count - 1));
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int count;
+	double num;
+
+	cout << "Введите значение x:" << endl << "x = ";
+	cin >> num;
+	cout << "Введите количество членов ряда" << endl;
+	cin >> count;
+	while (count < 1)
+	{
+		cout << "Введите корректное количество членов ряда (натуральное число)" << endl;
+		cin >> count;
+	}
+
+	cout << "Сумма членов ряда = " << get_sum(num, count-1);
+
+	return 0;
+}
+
+
